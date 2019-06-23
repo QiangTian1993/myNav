57e81c739f3a54fbfdf1cbe6b9bbc148f9dabfdd
Time <244439359@qq.com>
2019-06-23 18:49:31 +0800

    .

---
+++ js/main.js
@@ -0,0 +1,142 @@
+ //网址类型
+ let nav = ['设计网站', '前端开发', '其他']
+
+ //存放网址名称
+ let design = ["Dribbble", "Behance", "UI中国", "站酷", "优设网", "花瓣网"]
+ let web = ["阮一峰", "MDN", "张鑫旭", "Codrops", "CSS-TRICKS", "JavaScript标准参考", "掘金", "github.com"]
+ let other = ["bilibili", "腾讯视频", "爱奇艺", "喜马拉雅", "懒人听书", "荔枝FM"]
+
+ //存放网址
+
+ //设计网址
+ let designWebsite = {
+     'Dribbble': 'dribbble.com',
+     'Behance': 'www.behance.net',
+     'UI中国': 'www.ui.cn',
+     '站酷': 'www.zcool.com.cn',
+     '优设网': 'www.uisdc.com',
+     '花瓣网': 'huaban.com'
+ }
+
+ //前端开发
+ let webWebsite = {
+     '阮一峰': 'www.ruanyifeng.com/',
+     'MDN': 'developer.mozilla.org/',
+     '张鑫旭': 'zhangxinxu.com.cn',
+     'Codrops': 'tympanus.net/',
+     'CSS-TRICKS': 'css-tricks.com',
+     'JavaScript标准参考': 'javascript.ruanyifeng.com/',
+     '掘金': 'juejin.im/',
+     'github.com': 'github.com'
+ }
+
+ //其他
+ let otherWebsite = {
+     'bilibili': 'bilibili.com',
+     '腾讯视频': 'v.qq.com',
+     '爱奇艺': 'aiqiyi.com',
+     '喜马拉雅': 'ximalaya.com',
+     '懒人听书': 'lrts.me',
+     '荔枝FM': 'lizhi.fm'
+ }
+
+ //介绍
+
+ //设计网站
+ let designIntroduce = {
+     'Dribbble': '设计师作品分享平台，个人作品交流与分享',
+     'Behance': 'Adobe旗下的设计师交流平台，来自世界各地的设计师在这里分享自己的作品',
+     'UI中国': '图形交互与界面设计交流、作品展示、学习平台',
+     '站酷': '设计师交流平台，分享与学习的好去处',
+     '优设网': '国内优秀的设计师学习平台',
+     '花瓣网': '设计师寻找灵感的天堂'
+ }
+
+ //前端开发
+ let webIntroduce = {
+     '阮一峰': '阮一峰的个人博客',
+     'MDN': '网站提供开放网络(Open Web)技术有关的信息,包括用于网站和渐进式网络应用的 HTML、CSS 和 API',
+     '张鑫旭': '张鑫旭的个人博客',
+     'Codrops': '炫酷的CSS特效',
+     'CSS-TRICKS': '炫酷的CSS特效',
+     'JavaScript标准参考': 'JavaScript标准参考',
+     '掘金': '掘金是一个帮助开发者成长的社区',
+     'github': '开源及私有软件项目的托管平台'
+ }
+
+ //其他
+ let otherIntroduce = {
+     'bilibili': '国内知名的在线视频弹幕网站',
+     '腾讯视频': '中国领先的在线视频媒体平台',
+     '爱奇艺': '有海量、优质、高清的网络视频的大型视频网站,专业的网络视频播放平台',
+     '喜马拉雅': '专业的音频分享平台',
+     '懒人听书': '3.2亿用户选择的综合性有声阅读交流平台',
+     '荔枝FM': '业的音频分享平台'
+ }
+
+ //logo
+ let logo = {
+     'Dribbble': '/images/Dribbble.png',
+     'Behance': '/images/Behance.svg',
+     'UI中国': '/images/UI中国.png',
+     '站酷': '/images/站酷.jpeg',
+     '优设网': '/images/优设.png',
+     '花瓣网': '/images/花瓣.jpg',
+ }
+
+ //快捷键
+ let keyboard1 = ['D', 'B', 'U', 'Z', 'S', 'H']
+ let keyboard2 = ['R', 'M', 'X', 'C', 'T', 'J', 'CJ', 'G']
+ let keyboard3 = ['CB', 'CT', 'A', 'CX', 'CL', 'AL']
+
+ let designkbdWebsite = {
+     'd': 'dribbble.com',
+     'b': 'www.behance.net',
+     'u': 'www.ui.cn',
+     'z': 'www.zcool.com.cn',
+     's': 'www.uisdc.com',
+     'h': 'huaban.com',
+ }
+
+ let webkbdWebsite = {
+     'r': 'www.ruanyifeng.com/',
+     'm': 'developer.mozilla.org/',
+     'x': 'zhangxinxu.com.cn',
+     'c': 'tympanus.net/',
+     't': 'css-tricks.com',
+     'j': 'javascript.ruanyifeng.com/',
+     'cj': 'juejin.im/',
+     'g': 'github.com'
+ }
+
+ let otherkbdWebsite = {
+     'cb': 'bilibili.com',
+     'ct': 'v.qq.com',
+     'a': 'aiqiyi.com',
+     'cx': 'ximalaya.com',
+     'cl': 'www.lrts.me',
+     'al': 'lizhi.fm'
+ }
+
+ let userKbd = {
+     'd': 'dribbble.com',
+     'b': 'www.behance.net',
+     'u': 'www.ui.cn',
+     'z': 'www.zcool.com.cn',
+     's': 'www.uisdc.com',
+     'h': 'huaban.com',
+     'r': 'www.ruanyifeng.com/',
+     'm': 'developer.mozilla.org/',
+     'x': 'zhangxinxu.com.cn',
+     'c': 'tympanus.net/',
+     't': 'css-tricks.com',
+     'j': 'javascript.ruanyifeng.com/',
+     'cj': 'juejin.im/',
+     'g': 'github.com',
+     'cb': 'bilibili.com',
+     'ct': 'v.qq.com',
+     'a': 'aiqiyi.com',
+     'cx': 'ximalaya.com',
+     'cl': 'www.lrts.me',
+     '¬': 'lizhi.fm'
+ }
